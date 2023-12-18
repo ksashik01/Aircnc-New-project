858b6d65fc0b44f84f6f59cd3982830d2489c53d
ksashik01 <complainsheet@gmail.com>
2023-12-18 14:05:37 +0600

    new yyy

--- src/componesnts/Categories/CategoryBox.jsx
+++ src/componesnts/Categories/CategoryBox.jsx
@@ -1,10 +1,33 @@
 /* eslint-disable react/prop-types */
 
+import { Navigate, useNavigate, useSearchParams } from "react-router-dom";
+
 
 // eslint-disable-next-line no-unused-vars
 const CategoryBox = ({label,icon:Icon}) => {
+const [params, setParams] = useSearchParams()
+const value = params.get ('category')
+const navigate = useNavigate()
+
+const handleClick = () =>{
+    let currentQuery = {}
+    if (params) {
+        currentQuery = qs.parse(params.toString())
+    }
+    const updateQuery ={
+        ...currentQuery, category:label,
+    }
+
+    const url = qs.stringfyUrl({
+        url:'/',
+        query:updateQuery
+    },{skipNull:true})
+    navigate(url)
+}
+console.log(value)
+
     return (
-        <div className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500">
+        <div onClick={handleClick} className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500">
             <Icon size={26} />
             <div className="text-sm font-medium">
                 {label}
--- src/componesnts/Rooms/Rooms.jsx
+++ src/componesnts/Rooms/Rooms.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import Container from '../Container';
 import Card from './Card/Card';
 import Loader from '../Loader/Loader';
+import { useSearchParams } from 'react-router-dom';
 
 
 const Rooms = () => {
+    const [params, setParams] = useSearchParams()
+    const category = params.get('category')
     const [rooms, setRooms] = useState([])
     const [loading, setLoading] = useState(false)
 
