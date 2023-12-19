8e0de1ab5a5aee0114c79dd96a8ac280317335e4
ksashik01 <complainsheet@gmail.com>
2023-12-19 15:38:57 +0600

    link

--- src/componesnts/Categories/CategoryBox.jsx
+++ src/componesnts/Categories/CategoryBox.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
-import { Navigate, useNavigate, useSearchParams } from "react-router-dom";
+import qs from "query-string";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 
 // eslint-disable-next-line no-unused-vars
@@ -18,16 +19,18 @@ const handleClick = () =>{
         ...currentQuery, category:label,
     }
 
-    const url = qs.stringfyUrl({
+    const url = qs.stringifyUrl({
         url:'/',
         query:updateQuery
     },{skipNull:true})
+
     navigate(url)
 }
-console.log(value)
+
+
 
     return (
-        <div onClick={handleClick} className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500">
+        <div onClick={handleClick} className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500 cursor-pointer">
             <Icon size={26} />
             <div className="text-sm font-medium">
                 {label}
